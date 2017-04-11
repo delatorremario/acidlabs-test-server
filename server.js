1a29dd33790411fb7cb9600ccfb47bf3d30f78bc
Mario de la Torre <delatorremario@gmail.com>
2017-04-11 20:52:06 -0300

    remove host

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: '0.0.0.0', 
     port:+process.env.PORT || 5000,
     routes: { cors: true }   
 });
