22515f9fdce3fcd56ba1254991e72545f4cc34d2
Mario de la Torre <mariodelatorre@MacBook-Pro-de-MacPro.local>
2017-05-29 13:38:19 -0300

    last commit

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const Hapi = require('hapi');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ 
-    port:+process.env.PORT || 8000,
-    routes: { cors: true }   
+server.connection({
+    port: +process.env.PORT || 8000,
+    routes: { cors: true }
 });
 
 //Statics Pages
@@ -15,8 +15,8 @@ server.register([require('inert')], (err) => {
 
     // Start the server
     server.start(() => {
-        require('./lib/get_stock').init(server.listener,()=>{
-            console.log('Server running at:',server.info.uri);
+        require('./lib/get_stock').init(server.listener, () => {
+            console.log('Server running at:', server.info.uri);
         });
     });
 });
